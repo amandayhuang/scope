a46c5e2f7e5025044e15cffffa2f9487c52bba6f
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-07-02 17:21:21 -0400

    first commit

---
+++ api/getHoroscope.ts
@@ -0,0 +1,25 @@
+import { sql } from "@vercel/postgres";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const querySign = request.query.sign as string;
+  const todaysDateString = new Date().toISOString().split("T")[0];
+
+  const horoscopeRow =
+    await sql`SELECT * FROM horoscope where date(date) = ${todaysDateString} AND sign=${querySign} limit 1;`;
+
+  if (horoscopeRow?.rows.length !== 1) {
+    return response
+      .status(200)
+      .json({
+        date: todaysDateString,
+        sign: querySign,
+        horoscope: "not found",
+      });
+  }
+  const { sign, details: horoscope } = horoscopeRow.rows[0];
+  return response.status(200).json({ date: todaysDateString, sign, horoscope });
+}
