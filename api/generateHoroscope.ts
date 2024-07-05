d2847fdb15803dc056fe70e8f9c75e283c1dd244
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-07-05 15:47:36 -0400

    try simpler query

--- api/generateHoroscope.ts
+++ api/generateHoroscope.ts
@@ -56,40 +56,35 @@ export default async function handler(
   const voice = VOICES[Math.floor(Math.random() * VOICES.length)];
   const prompt = `The response should only contain the JSON object, nothing else. can you give me a horoscope for each sign for ${todaysDateString}. please format it as a JSON where each sign is a key and each value is another JSON with the date, sign, and horoscope of around 100 words in the voice of ${voice} (which will include the mention of a famous person with the same sign) as values, for example { "leo" : {date:"2024-07-02", sign: "leo", horscope: "the horscope longer than this:"} }`;
 
-  try {
-    const msg = await anthropic.messages.create({
-      model: "claude-instant-1.2",
-      max_tokens: 2500,
-      temperature: 0,
-      messages: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              text: prompt,
-            },
-          ],
-        },
-      ],
-    });
-  } catch (error) {
-    console.log("ERROR from claude", error);
-    return response.status(200).json({ count: -1 });
-  }
+  const msg = await anthropic.messages.create({
+    model: "claude-instant-1.2",
+    max_tokens: 2500,
+    temperature: 0,
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "what is 1 plus 1",
+          },
+        ],
+      },
+    ],
+  });
 
-  // console.log(msg);
+  console.log("claude RESP", msg);
   console.log("returned from claude");
   // @ts-ignore
-  const jsonResponse = JSON.parse(msg.content[0]?.text);
+  // const jsonResponse = JSON.parse(msg.content[0]?.text);
   let count = 0;
 
-  for (let i = 0; i < SIGNS.length; i++) {
-    const horoscope = jsonResponse[SIGNS[i]];
-    if (horoscope) {
-      await sql`INSERT INTO horoscope (date, sign, voice, details) VALUES (${horoscope.date}, ${horoscope.sign}, ${voice}, ${horoscope.horoscope})`;
-      count++;
-    }
-  }
+  // for (let i = 0; i < SIGNS.length; i++) {
+  //   const horoscope = jsonResponse[SIGNS[i]];
+  //   if (horoscope) {
+  //     await sql`INSERT INTO horoscope (date, sign, voice, details) VALUES (${horoscope.date}, ${horoscope.sign}, ${voice}, ${horoscope.horoscope})`;
+  //     count++;
+  //   }
+  // }
   return response.status(200).json({ count });
 }
