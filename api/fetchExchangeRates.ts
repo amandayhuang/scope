0d7aaf9a6f03a1d8740b76cb92523997d3411899
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-07-29 11:08:37 -0400

    schedule exchange rate job

--- api/fetchExchangeRates.ts
+++ api/fetchExchangeRates.ts
@@ -1,5 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getTodaysDateString } from "../services/util";
+import { sql } from "@vercel/postgres";
 
 export default async function handler(
   request: VercelRequest,
@@ -10,11 +11,13 @@ export default async function handler(
     "fca_live_2ayqQLg0cv4hLtImXA6ZLwJB7PGCl7HjoB5be21z";
   const url = "https://api.freecurrencyapi.com/v1/latest";
 
-  // const rowCount =
-  //   await sql`SELECT * FROM horoscope where date(date) = ${tomorrowDateString};`;
-  // if (horoscopeCount?.rowCount && horoscopeCount?.rowCount > 0) {
-  //   return response.status(200).json({ count: 0 });
-  // }
+  // return early if already have rates for today
+  const today = getTodaysDateString();
+  const exchangeCount =
+    await sql`SELECT * FROM exchange_rate where date(dt_created) = ${today};`;
+  if (exchangeCount?.rowCount && exchangeCount?.rowCount > 0) {
+    return response.status(200).json({ count: 0 });
+  }
 
   try {
     const resp = await fetch(url, {
@@ -24,6 +27,17 @@ export default async function handler(
       },
     });
     const data = await resp.json();
+
+    const dataArray = Object.entries(data.data);
+
+    for (let i = 0; i < dataArray.length; i++) {
+      const entry = dataArray[i];
+      const [k, v] = entry;
+      const rate = typeof v === "number" ? v : 0;
+
+      await sql`INSERT INTO exchange_rate (currency, rate) VALUES (${k}, ${rate})`;
+    }
+
     return response.status(200).json(data);
   } catch (error) {
     return response.status(500);
