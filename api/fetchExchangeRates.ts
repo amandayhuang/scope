fcbd01b67671070755333aaabcca835ecaddfc48
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-07-29 10:28:23 -0400

    fix fetch rates

--- api/fetchExchangeRates.ts
+++ api/fetchExchangeRates.ts
@@ -1,22 +1,30 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { getTodaysDateString } from "../services/util";
 
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  const key = process.env.CURRENCY_API_KEY;
+  const key =
+    process.env.CURRENCY_API_KEY ||
+    "fca_live_2ayqQLg0cv4hLtImXA6ZLwJB7PGCl7HjoB5be21z";
   const url = "https://api.freecurrencyapi.com/v1/latest";
 
+  // const rowCount =
+  //   await sql`SELECT * FROM horoscope where date(date) = ${tomorrowDateString};`;
+  // if (horoscopeCount?.rowCount && horoscopeCount?.rowCount > 0) {
+  //   return response.status(200).json({ count: 0 });
+  // }
+
   try {
-    const response = await fetch(url, {
+    const resp = await fetch(url, {
       method: "GET",
       headers: {
-        apikey: key || "",
+        apikey: key,
       },
     });
-    const data = await response.json();
-    console.log("DATA", data);
-    return data;
+    const data = await resp.json();
+    return response.status(200).json(data);
   } catch (error) {
     return response.status(500);
   }
